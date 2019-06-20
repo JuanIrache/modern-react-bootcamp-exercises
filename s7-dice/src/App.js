fbd27082f50b0e008a1272e384011cf06506fc55
JuanIrache <yrache@gmail.com>
2019-06-21 00:06:52 +0200

    section 7 exercise, roll dice

---
+++ s7-dice/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RollDice from './RollDice';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
