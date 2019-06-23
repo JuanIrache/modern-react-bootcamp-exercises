e56c44726f979032dbedf16dbf74a2cadef019f6
JuanIrache <yrache@gmail.com>
2019-06-23 22:42:39 +0200

    Add Icons

--- s16-yahtzee/src/Die.js
+++ s16-yahtzee/src/Die.js
@@ -6,15 +6,23 @@ class Die extends Component {
     super();
     this.click = this.click.bind(this);
   }
+
+  static defaultProps = {
+    icons: [
+      'fas fa-square',
+      'fas fa-dice-one',
+      'fas fa-dice-two',
+      'fas fa-dice-three',
+      'fas fa-dice-four',
+      'fas fa-dice-five',
+      'fas fa-dice-six'
+    ]
+  };
   click() {
     this.props.handleClick(this.props.idx);
   }
   render() {
-    return (
-      <button className={'Die'} style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }} onClick={this.click}>
-        {this.props.val}
-      </button>
-    );
+    return <i className={`Die ${this.props.icons[this.props.val || 0]}`} onClick={this.click} />;
   }
 }
 
