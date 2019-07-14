03da46c1520f1f3dfb0f8fc8170e730fcb9ff4e2
JuanIrache <yrache@gmail.com>
2019-07-14 21:54:52 +0200

    refactor palette list to functional component

--- s24-colors-app/src/PaletteList.js
+++ s24-colors-app/src/PaletteList.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import styles from './styles/PaletteListStyles';
 
-class PaletteList extends Component {
-  linkPalette = id => {
-    this.props.history.push(`/palette/${id}`);
+function PaletteList(props) {
+  const linkPalette = id => {
+    props.history.push(`/palette/${id}`);
   };
-  render() {
-    const { classes, deletePalette, resetPalettes } = this.props;
-    return (
-      <div className={classes.root}>
-        <div className={classes.container}>
-          <nav className={classes.nav}>
-            <h1>Palette Manager</h1>
-            <div>
-              <button onClick={resetPalettes}>Reset</button> | <Link to="/palette/new">Create Palette</Link>
-            </div>
-          </nav>
-          {this.props.palettes.map(p => (
-            <MiniPalette {...p} key={p.id} deletePalette={deletePalette} handleClick={this.linkPalette} />
-          ))}
-        </div>
+
+  const { classes, deletePalette, resetPalettes, palettes } = props;
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>Palette Manager</h1>
+          <div>
+            <button onClick={resetPalettes}>Reset</button> | <Link to="/palette/new">Create Palette</Link>
+          </div>
+        </nav>
+        {palettes.map(p => (
+          <MiniPalette {...p} key={p.id} deletePalette={deletePalette} handleClick={linkPalette} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withStyles(styles)(PaletteList);
