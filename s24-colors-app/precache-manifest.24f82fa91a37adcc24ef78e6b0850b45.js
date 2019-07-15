cd8fe49487754df08772f389b5eb45af88eda06a
JuanIrache <yrache@gmail.com>
2019-07-15 22:09:06 +0200

    Updates

---
+++ s24-colors-app/precache-manifest.24f82fa91a37adcc24ef78e6b0850b45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862bf986370eee07b5b3fe99103d7d4c",
+    "url": "/modern-react-bootcamp-exercises/s24-colors-app/index.html"
+  },
+  {
+    "revision": "295ffefe0906209b2723",
+    "url": "/modern-react-bootcamp-exercises/s24-colors-app/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "b64eb5149d7fc277ade5",
+    "url": "/modern-react-bootcamp-exercises/s24-colors-app/static/css/main.2edf2cd4.chunk.css"
+  },
+  {
+    "revision": "295ffefe0906209b2723",
+    "url": "/modern-react-bootcamp-exercises/s24-colors-app/static/js/2.9b6d0ada.chunk.js"
+  },
+  {
+    "revision": "b64eb5149d7fc277ade5",
+    "url": "/modern-react-bootcamp-exercises/s24-colors-app/static/js/main.a3f63b68.chunk.js"
+  },
+  {
+    "revision": "8385411bc411ac05b529",
+    "url": "/modern-react-bootcamp-exercises/s24-colors-app/static/js/runtime~main.0e02e0a0.js"
+  },
+  {
+    "revision": "77bfb63725d58295116c5ebaf035e875",
+    "url": "/modern-react-bootcamp-exercises/s24-colors-app/static/media/bg.77bfb637.svg"
+  }
+]);
