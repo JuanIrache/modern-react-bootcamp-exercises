8bc940883ed1ec543da105a51c8ade7e6b75d16e
JuanIrache <yrache@gmail.com>
2019-07-15 21:32:43 +0200

    build for pages

---
+++ s24-colors-app/build/precache-manifest.06b66f0b8b85b71fa8b42342d9bf86a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff08fff94f6cb4ffd4c52f52129c2ce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e36b34f5a93888da341",
+    "url": "/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "b64eb5149d7fc277ade5",
+    "url": "/static/css/main.2edf2cd4.chunk.css"
+  },
+  {
+    "revision": "4e36b34f5a93888da341",
+    "url": "/static/js/2.ea03b76a.chunk.js"
+  },
+  {
+    "revision": "b64eb5149d7fc277ade5",
+    "url": "/static/js/main.a3f63b68.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "77bfb63725d58295116c5ebaf035e875",
+    "url": "/static/media/bg.77bfb637.svg"
+  }
+]);
