c22799744f91fbd480aacaac72f1ed431c511c69
JuanIrache <yrache@gmail.com>
2019-07-17 01:48:16 +0200

    starting hooks app

---
+++ s33-login_page/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Todo from './Todo';
+
+const App = () => {
+  return <Todo />;
+};
+
+export default App;
---
+++ s33-login_page/src/styles/TodoStyles.js
@@ -0,0 +1,62 @@
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  flag: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    // flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  switch: {
+    margin:'auto'
+    // marginLeft:0,
+    // marginRight:'auto'
+  },
+  p:{
+    display:'inline'
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200
+      }
+    }
+  }
+}));
