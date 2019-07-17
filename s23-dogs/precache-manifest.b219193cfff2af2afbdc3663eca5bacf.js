62d341789fbb76a9d8f8a9fad0796f65ad859ec4
JuanIrache <yrache@gmail.com>
2019-07-17 23:20:36 +0200

    Updates

---
+++ s23-dogs/precache-manifest.b219193cfff2af2afbdc3663eca5bacf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e416dd338ed5ba88c847b6e5b09245fe",
+    "url": "/modern-react-bootcamp-exercises/s23-dogs/index.html"
+  },
+  {
+    "revision": "0f675c30a2b1af32e944",
+    "url": "/modern-react-bootcamp-exercises/s23-dogs/static/css/main.e728c192.chunk.css"
+  },
+  {
+    "revision": "92f816f44cd440e150d3",
+    "url": "/modern-react-bootcamp-exercises/s23-dogs/static/js/2.858658a2.chunk.js"
+  },
+  {
+    "revision": "0f675c30a2b1af32e944",
+    "url": "/modern-react-bootcamp-exercises/s23-dogs/static/js/main.d566bf87.chunk.js"
+  },
+  {
+    "revision": "bb8e89fb6bdad3924d1a",
+    "url": "/modern-react-bootcamp-exercises/s23-dogs/static/js/runtime~main.c255dd0d.js"
+  }
+]);
