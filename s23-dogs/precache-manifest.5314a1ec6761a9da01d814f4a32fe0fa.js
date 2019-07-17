0830b7a589da4c4434d340b12b71ee22c59f13a8
JuanIrache <yrache@gmail.com>
2019-07-17 23:50:40 +0200

    Updates

--- s23-dogs/precache-manifest.5314a1ec6761a9da01d814f4a32fe0fa.js
+++ s23-dogs/precache-manifest.5314a1ec6761a9da01d814f4a32fe0fa.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e622a227c42ec8fe10b7301957fcc28",
+    "revision": "064e5efaf84651c35b177842e1964c1f",
     "url": "/modern-react-bootcamp-exercises/s23-dogs/index.html"
   },
   {
-    "revision": "ce96dc9ff2f9f60f0445",
-    "url": "/modern-react-bootcamp-exercises/s23-dogs/static/css/main.e728c192.chunk.css"
+    "revision": "e8ccb37fe77e484f3cb4",
+    "url": "/modern-react-bootcamp-exercises/s23-dogs/static/css/main.8e51a023.chunk.css"
   },
   {
     "revision": "8fc7768c8e9fc90a9fc5",
     "url": "/modern-react-bootcamp-exercises/s23-dogs/static/js/2.79a8c86e.chunk.js"
   },
   {
-    "revision": "ce96dc9ff2f9f60f0445",
-    "url": "/modern-react-bootcamp-exercises/s23-dogs/static/js/main.ccfb3154.chunk.js"
+    "revision": "e8ccb37fe77e484f3cb4",
+    "url": "/modern-react-bootcamp-exercises/s23-dogs/static/js/main.886e39c2.chunk.js"
   },
   {
     "revision": "bb8e89fb6bdad3924d1a",
--- s23-dogs/service-worker.js
+++ s23-dogs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/modern-react-bootcamp-exercises/s23-dogs/precache-manifest.32869e8b52c3dbb70373c2f767335845.js"
+  "/modern-react-bootcamp-exercises/s23-dogs/precache-manifest.5314a1ec6761a9da01d814f4a32fe0fa.js"
 );
 
 self.addEventListener('message', (event) => {
