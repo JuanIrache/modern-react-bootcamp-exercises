cd4b7d600c96bc35b955412e8a37be14e25a279e
JuanIrache <yrache@gmail.com>
2019-07-18 00:03:22 +0200

    fix back for desktop

--- s23-dogs/src/index.js
+++ s23-dogs/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
