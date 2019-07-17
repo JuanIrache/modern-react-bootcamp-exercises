e51f57c12ccf9535a7e0491cc46acca9470be33a
JuanIrache <yrache@gmail.com>
2019-07-17 23:50:15 +0200

    fix css on mobile

--- s23-dogs/src/Dog.js
+++ s23-dogs/src/Dog.js
@@ -8,25 +8,27 @@ export default class Dog extends Component {
       const { name, age, facts, src } = this.props.dog;
       return (
         <div className="Dog">
-          <img className="Dog-img" src={src} alt={name} />
-          <div className="Dog-description">
-            <div className="Dog-description-left">
-              <h1 className="Dog-name">{name}</h1>
-              <h3 className="Dog-age">{age} years old</h3>
-            </div>
-            <div className="Dog-description-middle">
-              <ul className="Dog-facts">
-                {facts.map((f, i) => (
-                  <li className="Dog-fact" key={i}>
-                    {f}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="Dog-description-right">
-              <button className="Dog-button" onClick={this.props.history.goBack}>
-                Back
-              </button>
+          <div className="Dog-inner">
+            <img className="Dog-img" src={src} alt={name} />
+            <div className="Dog-description">
+              <div className="Dog-description-left">
+                <h1 className="Dog-name">{name}</h1>
+                <h3 className="Dog-age">{age} years old</h3>
+              </div>
+              <div className="Dog-description-middle">
+                <ul className="Dog-facts">
+                  {facts.map((f, i) => (
+                    <li className="Dog-fact" key={i}>
+                      {f}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="Dog-description-right">
+                <button className="Dog-button" onClick={this.props.history.goBack}>
+                  Back
+                </button>
+              </div>
             </div>
           </div>
         </div>
