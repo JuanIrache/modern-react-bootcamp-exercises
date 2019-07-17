8e192921df1fece6e7a20dd59a0d9566bb85ebbd
JuanIrache <yrache@gmail.com>
2019-07-17 23:00:37 +0200

    Updates

---
+++ s33-login_page/precache-manifest.5408ea017c2a0510dffde01c0405951c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206ffb5900732237df41761315e6e556",
+    "url": "/modern-react-bootcamp-exercises/s33-login_page/index.html"
+  },
+  {
+    "revision": "21142f7a3302053428d2",
+    "url": "/modern-react-bootcamp-exercises/s33-login_page/static/css/main.6c361192.chunk.css"
+  },
+  {
+    "revision": "bf342c13c478458d3156",
+    "url": "/modern-react-bootcamp-exercises/s33-login_page/static/js/2.5e8b4c77.chunk.js"
+  },
+  {
+    "revision": "21142f7a3302053428d2",
+    "url": "/modern-react-bootcamp-exercises/s33-login_page/static/js/main.559aac1a.chunk.js"
+  },
+  {
+    "revision": "e3874b8a57341b83e53c",
+    "url": "/modern-react-bootcamp-exercises/s33-login_page/static/js/runtime~main.041b303a.js"
+  }
+]);
