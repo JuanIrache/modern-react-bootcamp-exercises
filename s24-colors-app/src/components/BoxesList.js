5dd28096a3ecd8c4a98224784e5e0e9ae3959f4d
JuanIrache <yrache@gmail.com>
2019-07-17 15:42:02 +0200

    silly animations when generating and sorting

--- s24-colors-app/src/components/BoxesList.js
+++ s24-colors-app/src/components/BoxesList.js
@@ -13,7 +13,7 @@ function BoxesList({ colors, removeColor, classes, duplicateColor, toggleLock })
   return (
     <div className={classes.root}>
       {colors.map((col, i) => (
-        <DraggableBox key={col.name} index={i} {...{ duplicateColor, removeColor, toggleLock }} {...col} />
+        <DraggableBox index={i} {...{ duplicateColor, removeColor, toggleLock }} {...col} />
       ))}
     </div>
   );
--- s24-colors-app/src/components/NewPalette.js
+++ s24-colors-app/src/components/NewPalette.js
@@ -13,7 +13,7 @@ class NewPalette extends Component {
     open: true,
     color: 'pink',
     name: '',
-    colors: this.props.startingColors.map(c => ({ ...c, locked: false })),
+    colors: this.props.startingColors.map(c => ({ ...c, locked: false, key: c.name })),
     paletteName: '',
     emoji: ''
   };
@@ -47,7 +47,7 @@ class NewPalette extends Component {
   };
 
   addColor = () => {
-    const newColor = { name: this.state.name, color: this.state.color, locked: false };
+    const newColor = { name: this.state.name, color: this.state.color, locked: false, key: this.state.name };
     this.setState({ colors: [...this.state.colors, newColor], name: '' });
   };
 
@@ -62,7 +62,7 @@ class NewPalette extends Component {
     do {
       newColor = smartColorGenerator(basedOn);
     } while (repeated(newColor));
-    return { ...newColor, locked: false };
+    return { ...newColor, locked: false, key: newColor.name };
   };
 
   autoColor = () => {
@@ -87,30 +87,52 @@ class NewPalette extends Component {
   };
 
   //Drag and drop sort!
-  onSortEnd = ({ oldIndex: oldI, newIndex: newI }) => {
+  onSortEnd = ({ oldIndex: oldI, newIndex: newI, cb }) => {
     const { colors } = this.state;
     const movingColor = colors[oldI];
     const backwards = oldI > newI;
-    let newOrder;
+    let newOrder = [];
     if (backwards) {
       newOrder = [...colors.slice(0, newI), movingColor, ...colors.slice(newI, oldI), ...colors.slice(oldI + 1)];
     } else {
       newOrder = [...colors.slice(0, oldI), ...colors.slice(oldI + 1, newI + 1), movingColor, ...colors.slice(newI + 1)];
     }
-    this.setState({ colors: newOrder });
+    this.setState({ colors: newOrder }, () => {
+      if (cb) cb();
+    });
   };
 
   autoSort = () => {
+    const oldOrder = JSON.parse(JSON.stringify(this.state.colors));
     const dist = (col, ref) => chroma.deltaE(ref, col);
     const temp = col => chroma(col).temperature();
-    const firstReorder = this.state.colors.sort((a, b) => temp(a.color) - temp(b.color));
+    const firstReorder = oldOrder.sort((a, b) => temp(a.color) - temp(b.color));
     let newOrder = [];
     for (let i = 0; i < 4; i++) {
       const section = firstReorder.slice(i * 5, i * 5 + 5);
       newOrder = newOrder.concat(section.sort((a, b) => dist(a.color, section[0].color) - dist(b.color, section[0].color)));
     }
-    this.setState({ colors: newOrder });
+    this.sortOneByOne({ oldOrder, newOrder, idx: 0 });
   };
+
+  sortOneByOne({ oldOrder, newOrder, idx }) {
+    if (oldOrder.length > idx) {
+      const key = newOrder[idx].key;
+      const oldIndex = this.state.colors.reduce((acc, cur, i) => (cur.key === key ? i : acc), 0);
+      const newIndex = newOrder.reduce((acc, cur, i) => (cur.key === key ? i : acc), 0);
+      this.onSortEnd({
+        oldIndex,
+        newIndex,
+        cb: () => {
+          if (idx < 19) {
+            setImmediate(() => {
+              this.sortOneByOne({ oldOrder, newOrder, idx: idx + 1 });
+            });
+          }
+        }
+      });
+    }
+  }
 
   autoGenerate = () => {
     let baseColors = this.state.colors.filter(c => c.locked);
@@ -126,7 +148,31 @@ class NewPalette extends Component {
         newColors[i] = newColor;
       }
     }
-    this.setState({ colors: newColors });
+
+    //Use a temporary array with different keys and empty colors to allow updating one by one without duplicating keys
+    let tempColors = [];
+    for (let i = 0; i < 20; i++) {
+      let existing = this.state.colors[i];
+      if (existing) {
+        tempColors[i] = existing.locked ? existing : { ...existing, key: i + 'temp' };
+      } else {
+        tempColors[i] = { name: '', color: 'rgba(0,0,0,0)', key: i + 'temp' };
+      }
+    }
+
+    this.setState({ colors: tempColors }, () => {
+      this.updateOneByOne(newColors, 0);
+    });
+  };
+
+  updateOneByOne = (colors, idx) => {
+    this.setState({ colors: this.state.colors.map((c, i) => (i === idx ? colors[idx] : c)) }, () => {
+      if (idx < 19) {
+        setImmediate(() => {
+          this.updateOneByOne(colors, idx + 1);
+        });
+      }
+    });
   };
 
   duplicateColor = color => {
