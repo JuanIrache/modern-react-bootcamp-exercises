346a37d59383638f9cb49d8f4cbac2eceb5ee2ba
JuanIrache <yrache@gmail.com>
2019-07-18 03:33:36 +0200

    Updates

---
+++ s18-deck-of-cards/precache-manifest.2635ed1b937e464da24878e66d273be8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623b425afb2e322390cad153b443b202",
+    "url": "/modern-react-bootcamp-exercises/s18-deck-of-cards/index.html"
+  },
+  {
+    "revision": "ee2d1245722fd18850dd",
+    "url": "/modern-react-bootcamp-exercises/s18-deck-of-cards/static/css/main.58d6feb2.chunk.css"
+  },
+  {
+    "revision": "2f05a16312a5c5e3f1fe",
+    "url": "/modern-react-bootcamp-exercises/s18-deck-of-cards/static/js/2.ce89f1f7.chunk.js"
+  },
+  {
+    "revision": "ee2d1245722fd18850dd",
+    "url": "/modern-react-bootcamp-exercises/s18-deck-of-cards/static/js/main.b0960b90.chunk.js"
+  },
+  {
+    "revision": "171dda07fe853e9fb56f",
+    "url": "/modern-react-bootcamp-exercises/s18-deck-of-cards/static/js/runtime~main.6aff7520.js"
+  }
+]);
