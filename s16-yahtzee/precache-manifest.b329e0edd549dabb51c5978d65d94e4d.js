afd9d24c68e833c7d8cde41bfd15d3feee596943
JuanIrache <yrache@gmail.com>
2019-07-18 17:16:00 +0200

    Updates

---
+++ s16-yahtzee/precache-manifest.b329e0edd549dabb51c5978d65d94e4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8961ba7900d448572851fdb2300cfa16",
+    "url": "/modern-react-bootcamp-exercises/s16-yahtzee/index.html"
+  },
+  {
+    "revision": "9b04aae815f6346d9578",
+    "url": "/modern-react-bootcamp-exercises/s16-yahtzee/static/css/main.2dd6b9c4.chunk.css"
+  },
+  {
+    "revision": "a3efb3cea2e822a9da2e",
+    "url": "/modern-react-bootcamp-exercises/s16-yahtzee/static/js/2.47856582.chunk.js"
+  },
+  {
+    "revision": "9b04aae815f6346d9578",
+    "url": "/modern-react-bootcamp-exercises/s16-yahtzee/static/js/main.e2d5d67e.chunk.js"
+  },
+  {
+    "revision": "f71a5c887b33d7ac860d",
+    "url": "/modern-react-bootcamp-exercises/s16-yahtzee/static/js/runtime~main.77c40d73.js"
+  }
+]);
