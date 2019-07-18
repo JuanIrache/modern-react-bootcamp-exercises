6456c2eed39cd52eac9f393437544f4c1d189e01
JuanIrache <yrache@gmail.com>
2019-07-18 04:05:05 +0200

    Updates

---
+++ s15-todo-list/precache-manifest.b5f710d6e99545ae71dbff15b98db2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4691bed6605c07e661b499b4d3e3786e",
+    "url": "/modern-react-bootcamp-exercises/s15-todo-list/index.html"
+  },
+  {
+    "revision": "14ac172ded9b8fa47f53",
+    "url": "/modern-react-bootcamp-exercises/s15-todo-list/static/css/main.4b91003d.chunk.css"
+  },
+  {
+    "revision": "3a78d3f5e8d66730cdf0",
+    "url": "/modern-react-bootcamp-exercises/s15-todo-list/static/js/2.8199aca0.chunk.js"
+  },
+  {
+    "revision": "14ac172ded9b8fa47f53",
+    "url": "/modern-react-bootcamp-exercises/s15-todo-list/static/js/main.275eb06d.chunk.js"
+  },
+  {
+    "revision": "8b0b54464bd37c356961",
+    "url": "/modern-react-bootcamp-exercises/s15-todo-list/static/js/runtime~main.beaa4a56.js"
+  },
+  {
+    "revision": "4f96b4e8e1bc3ca37ea4d86e1bd01960",
+    "url": "/modern-react-bootcamp-exercises/s15-todo-list/static/media/bg.4f96b4e8.svg"
+  }
+]);
