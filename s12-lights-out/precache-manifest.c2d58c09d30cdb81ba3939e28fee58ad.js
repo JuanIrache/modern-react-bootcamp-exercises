21a2edbba50a15301002a1f33b63cd6d94faeb9d
JuanIrache <yrache@gmail.com>
2019-07-18 04:25:35 +0200

    Updates

---
+++ s12-lights-out/precache-manifest.c2d58c09d30cdb81ba3939e28fee58ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d51e37dd05840ab2ec8",
+    "url": "/modern-react-bootcamp-exercises/s12-lights-out/static/js/runtime~main.4d51e37d.js"
+  },
+  {
+    "revision": "3ee0328b089a462402f3",
+    "url": "/modern-react-bootcamp-exercises/s12-lights-out/static/js/main.3ee0328b.chunk.js"
+  },
+  {
+    "revision": "e614b0c7a408a3da68bb",
+    "url": "/modern-react-bootcamp-exercises/s12-lights-out/static/js/1.e614b0c7.chunk.js"
+  },
+  {
+    "revision": "3ee0328b089a462402f3",
+    "url": "/modern-react-bootcamp-exercises/s12-lights-out/static/css/main.8315b824.chunk.css"
+  },
+  {
+    "revision": "39afce9dd19bc32dc232ad64c087f834",
+    "url": "/modern-react-bootcamp-exercises/s12-lights-out/index.html"
+  }
+];
